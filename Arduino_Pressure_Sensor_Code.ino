f8a074f2246e67d1ed85f7c8327683771a684144
Ji ZunLiang <96689128+Jake4REAL@users.noreply.github.com>
2021-12-27 16:38:33 +0800

    Update Arduino_Pressure_Sensor_Code.ino

--- Arduino_Pressure_Sensor_Code.ino
+++ Arduino_Pressure_Sensor_Code.ino
@@ -1,12 +1,12 @@
 int fsrPin = 0;  //  
 int fsrReading;   //
 int fsrVoltage; //
-unsigned long fsrResistance; // The voltage converted to resistance, can be very big so make "lo
+unsigned long fsrResistance; // 
 unsigned long fsrConductance;
-long fsrForce;  // Finally, the resistance converted to force
+long fsrForce;  // 
 
 void setup(void) {
-Serial.begin(9600);  // We'll send debugging information via the Serial monitor
+Serial.begin(9600);  //
 }
 
 void loop(void){
@@ -14,7 +14,6 @@ fsrReading = analogRead(fsrPin);
 Serial.print("Analog reading = "); 
 Serial.println(fsrReading);
 
-// analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
 fsrVoltage = map(fsrReading, 0, 1023, 0, 5000); 
 Serial.print("Voltage reading in mV = "); 
 Serial.println(fsrVoltage); 
@@ -24,20 +23,17 @@ if (fsrVoltage == 0) {
 Serial.println("No  pressure");
 } 
 else {
-// The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
-// so FSR = ((Vcc - V) * R) / V yay math!
-fsrResistance = 5000 - fsrVoltage;  // fsrVoltage is in millivolts so 5V = 5000mV
-fsrResistance *= 10000; // 10K resistor
+fsrResistance = 5000 - fsrVoltage;  // 
+fsrResistance *= 10000; // 
 fsrResistance /= fsrVoltage; 
 Serial.print("FSR resistance in ohms = "); 
 Serial.println(fsrResistance);
 
-fsrConductance = 1000000; // we measure in micromhos so
+fsrConductance = 1000000; // 
 fsrConductance /= fsrResistance; 
 Serial.print("Conductance in microMhos: "); 
 Serial.println(fsrConductance);
 
-// Use the two FSR guide graphs to approximate the force
 if (fsrConductance <= 1000) 
 { fsrForce = fsrConductance / 80; 
 Serial.print("Force in Newtons: "); 
